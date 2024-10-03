b36c4dcb8a5eb0c7bd301ed48a4a66ef215f80bb
Kelechi Otugbuali <hilakechi@gmail.com>
2024-10-04 00:13:31 +0100

    Card component removed

--- src/_root/pages/landing/Landing.tsx
+++ src/_root/pages/landing/Landing.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import LandingHero from '../../../components/landingHero/LandingHero'
-import Card from '../../../components/card/Card'
 
 const Landing = () => {
   return (
     <section data-testid="landing-hero">
       <LandingHero />
-      <Card />
     </section>
   )
 }
