0b65cffadd9f19c1c272f559775c7c1f47c87634
Kelechi Otugbuali <hilakechi@gmail.com>
2024-10-18 19:32:29 +0100

    fixed pr comment issue

--- src/components/personalisedRecommendation/PersonalisedRecommendation.tsx
+++ src/components/personalisedRecommendation/PersonalisedRecommendation.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import styles from './PersonalisedRecommendation.module.scss'
 
 const PersonalisedRecommendation = () => {
-  console.log(styles)
   return (
     <section
       data-testid="personalised-recommendation"
