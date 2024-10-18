32a8c03b942708a80dd31a27e6183ae76109e4bf
Kelechi Otugbuali <hilakechi@gmail.com>
2024-10-18 21:30:55 +0100

    add the text in the footer section

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,19 +1,20 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import { Navbar } from '../components'
+import { Footer, Navbar } from '../components'
 
 interface RootLayoutProps {
   children?: React.ReactNode
 }
 
-const RootLayout = ({ children } : RootLayoutProps) => {
+const RootLayout = ({ children }: RootLayoutProps) => {
   return (
     <div className="">
-      <Navbar/>
+      <Navbar />
       <main>
         {children}
         <Outlet /> {/* This will render nested routes */}
       </main>
+      <Footer />
     </div>
   )
 }
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,14 @@
 import styles from './Footer.module.scss'
 
 const Footer = () => {
-  return <footer className={styles.footer}>Footer</footer>
+  return (
+    <footer className={styles.footer}>
+      <img src="./assets/footerStroke.png" alt="footerStroke" />
+      <div className={styles.footer__container}>
+        <h3>The Best spot for all your fresh buys</h3>
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
